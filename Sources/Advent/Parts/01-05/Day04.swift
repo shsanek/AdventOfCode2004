1a544a3741220bee2f1ca9c64d6ad60286c6c2de
Alexandr Shipin <ssanek212@gmail.com>
2024-12-04 07:30:04 +0200

    Day 4

--- Sources/Advent/Parts/01-05/Day04.swift
+++ Sources/Advent/Parts/01-05/Day04.swift
@@ -1,11 +1,83 @@
 import Foundation
 
+func check(x: Int, y: Int, rows: [[String]], directX: Int, directY: Int, text: [String]) -> Bool {
+    for i in 0..<text.count {
+        if rows[y + directY * i][x + directX * i] != text[i] {
+            return false
+        }
+    }
+    return true
+}
+
 func day04part1() -> Int {
-    let input = DataLoader.load()
-    return 0
+    let text = "XMAS".toArray
+    let horizontalOffset = Array(repeating: "_", count: text.count).joined()
+    var rows = DataLoader.load()
+        .split(separator: "\n")
+        .map({ String(horizontalOffset + $0 + horizontalOffset).removeSpace.toArray })
+    let verticalOffset = Array(repeating: Array(repeating: "_", count: rows[0].count), count: text.count)
+    rows = verticalOffset + rows + verticalOffset
+
+    var result = 0
+    for y in text.count..<(rows.count - text.count) {
+        for x in text.count..<(rows[0].count - text.count) {
+            guard rows[y][x] == text[0] else {
+                continue
+            }
+            if check(x: x, y: y, rows: rows, directX: 1, directY: 0, text: text) {
+                result += 1
+            }
+            if check(x: x, y: y, rows: rows, directX: -1, directY: 0, text: text) {
+                result += 1
+            }
+            if check(x: x, y: y, rows: rows, directX: 0, directY: 1, text: text) {
+                result += 1
+            }
+            if check(x: x, y: y, rows: rows, directX: 0, directY: -1, text: text) {
+                result += 1
+            }
+            if check(x: x, y: y, rows: rows, directX: 1, directY: -1, text: text) {
+                result += 1
+            }
+            if check(x: x, y: y, rows: rows, directX: -1, directY: -1, text: text) {
+                result += 1
+            }
+            if check(x: x, y: y, rows: rows, directX: 1, directY: 1, text: text) {
+                result += 1
+            }
+            if check(x: x, y: y, rows: rows, directX: -1, directY: 1, text: text) {
+                result += 1
+            }
+        }
+    }
+
+    return result
 }
 
 func day04part2() -> Int {
-    let input = DataLoader.load()
-    return 0
+    let text = "MAS".toArray
+    let horizontalOffset = Array(repeating: "_", count: text.count).joined()
+    var rows = DataLoader.load()
+        .split(separator: "\n")
+        .map({ String(horizontalOffset + $0 + horizontalOffset).removeSpace.toArray })
+    let verticalOffset = Array(repeating: Array(repeating: "_", count: rows[0].count), count: text.count)
+    rows = verticalOffset + rows + verticalOffset
+
+    var result = 0
+    let set = Set(["M", "S"])
+    for y in text.count..<(rows.count - text.count) {
+        for x in text.count..<(rows[0].count - text.count) {
+            guard rows[y][x] == "A" else {
+                continue
+            }
+            let first = Set([rows[y - 1][x - 1], rows[y + 1][x + 1]])
+            let second = Set([rows[y - 1][x + 1], rows[y + 1][x - 1]])
+
+            if set == first, set == second {
+                result += 1
+            }
+        }
+    }
+
+    return result
 }
--- Tests/AdventTests/01-05/Day04Test.swift
+++ Tests/AdventTests/01-05/Day04Test.swift
@@ -3,13 +3,23 @@ import XCTest
 
 final class Day04Test: XCTestCase, IBaseTest {
     let dataTest = """
+    MMMSXXMASM
+    MSAMXMSMSA
+    AMXSXMAAMM
+    MSAMASMSMX
+    XMASAMXAMM
+    XXAMMXXAMA
+    SMSMSASXSS
+    SAXAMASAAA
+    MAMMMXMMMM
+    MXMXAXMASX
     """
 
     func testPart1() throws {
-        check(testValue: .unowned, day04part1)
+        check(testValue: 18, day04part1)
     }
 
     func testPart2() throws {
-        check(testValue: .unowned, day04part2)
+        check(testValue: 9, day04part2)
     }
 }
