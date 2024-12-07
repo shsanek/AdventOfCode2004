f648a61d58178684c54c42b3d3c8f5836b11df60
Alexandr Shipin <ssasnek212@gmail.com>
2024-12-07 07:47:39 +0200

    Day 7

--- Tests/AdventTests/06-10/Day07Test.swift
+++ Tests/AdventTests/06-10/Day07Test.swift
@@ -3,13 +3,22 @@ import XCTest
 
 final class Day07Test: XCTestCase, IBaseTest {
     let dataTest = """
+    190: 10 19
+    3267: 81 40 27
+    83: 17 5
+    156: 15 6
+    7290: 6 8 6 15
+    161011: 16 10 13
+    192: 17 8 14
+    21037: 9 7 18 13
+    292: 11 6 16 20
     """
 
     func testPart1() throws {
-        check(testValue: .unowned, day07part1)
+        check(testValue: 3749, day07part1)
     }
 
     func testPart2() throws {
-        check(testValue: .unowned, day07part2)
+        check(testValue: 11387, day07part2)
     }
 }
