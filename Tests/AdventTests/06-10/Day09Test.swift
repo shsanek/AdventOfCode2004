23bc41c6e54d9de2af7b1218d272037357650dbb
Alexandr Shipin <alexandr.shipin@indriver.com>
2024-12-09 08:33:24 +0200

    Day 9

--- Tests/AdventTests/06-10/Day09Test.swift
+++ Tests/AdventTests/06-10/Day09Test.swift
@@ -3,13 +3,14 @@ import XCTest
 
 final class Day09Test: XCTestCase, IBaseTest {
     let dataTest = """
+    2333133121414131402
     """
 
     func testPart1() throws {
-        check(testValue: .unowned, day09part1)
+        check(testValue: 1928, day09part1)
     }
 
     func testPart2() throws {
-        check(testValue: .unowned, day09part2)
+        check(testValue: 2858, day09part2)
     }
 }
